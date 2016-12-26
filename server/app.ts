3b45d446dd77f32e15ca913c7618931d22c4ff59
tongjinle <1328192478@qq.com>
2016-12-27 01:07:53 +0800

    完成app.ts(未经测试)

--- server/app.ts
+++ server/app.ts
@@ -17,16 +17,17 @@ import Game from './game';
 import User from './user';
 import CONFIG from './config';
 
+type Info = { rid: string, gid: string, uid: string };
 
 class App {
-	private gameDict: { [sid: string]: any };
-	private userInfoDict: { [sid: string]: any };
+	// 记录映射信息
+	private dict: { [sid: string]: EnterRoomData };
 
-	private waitRoomDict;
+	// 游戏实例列表
 	gameList: Game[];
 	constructor() {
-		this.waitRoomDict = {}
 		this.gameList = [];
+		this.dict = {};
 
 		let app = Http.createClient();
 		let io = SocketIO(app);
@@ -36,26 +37,64 @@ class App {
 		this.bind(io);
 	}
 
+	private joinRoom(sid: string, data: EnterRoomData) {
+		this.dict[sid] = this.dict[sid] || data;
+	}
+
+	private leaveRoom(sid: string) {
+		delete this.dict[sid];
+	}
+
+	private getRoom(sid: string): EnterRoomData {
+		return this.dict[sid];
+	}
+
+	private getUserListInRoom(gameId: string): EnterRoomData[] {
+		let userList: EnterRoomData[] = [];
+		_.each(this.dict, (us, sid) => {
+			if (us.gameId == gameId) {
+				userList.push(us);
+			}
+		});
+		return userList;
+	}
+
+
+	private getGame(sid: string): Game {
+		return _.find(this.gameList, ga => ga.id == this.getRoom(sid).gameId);
+	}
+
+	private getUserId(sid: string): string {
+		return this.getRoom(sid).userId;
+	}
+
 	bind(io: SocketIO.Server) {
 		io.on('connection', (so) => {
 			// 进入房间
 			so.on(RequestType.enterRoom, (data: EnterRoomData) => {
-				let ro = this.waitRoomDict[data.gameId] = this.waitRoomDict[data.gameId] = [];
-				ro.push(data);
-
+				let sid = so.id;
+				let gameId  = data.gameId;
+				// 记录sid对应的信息
+				this.joinRoom(sid, data);
 				so.join(data.gameId);
-				so.emit(PushType.onenterRoom, data);
+
+				io.to(gameId).emit(PushType.onenterRoom, data);
 
 				// 查看游戏能否开始
-				if (ro.length == CONFIG.userCount) {
-					let userList = _.map(ro, (d, index) => {
+				let room = this.getRoom(sid);
+				let list = this.getUserListInRoom(gameId);
+				let userCount: number = list.length;
+				if (userCount == CONFIG.userCount) {
+					let userList = _.map(list, (d, index) => {
 						let us = new User();
 						us.animal = index == 0 ? Animal.cat : Animal.dog;
 						return us;
 					});
-					let ga = new Game(data.gameId, userList);
+					let ga = new Game(gameId, userList);
+					this.gameList.push(ga);
 
-					so.emit(PushType.ongameStart, { userList: ga.userList });
+					// 广播"游戏开始"
+					io.to(gameId).emit(PushType.ongameStart, { userList: ga.userList });
 				}
 			});
 
@@ -65,7 +104,9 @@ class App {
 				let cupIndex = data.cupIndex;
 				let flag = ga.putMouse(userId, cupIndex);
 
-				so.emit(PushType.onputMouse, { flag });
+				let gameId = ga.id;
+
+				io.to(gameId).emit(PushType.onputMouse, { flag });
 			});
 
 			so.on(RequestType.rollCup, (data: RollCupData) => {
@@ -74,7 +115,9 @@ class App {
 				let posiList = data.posiList;
 				let flag = ga.rollCup(userId, posiList);
 
-				so.emit(PushType.onrollCup, { flag });
+				let gameId = ga.id;
+
+				io.to(gameId).emit(PushType.onrollCup, { flag });
 
 			});
 
@@ -82,40 +125,39 @@ class App {
 				let ga = this.getGame(so.id);
 				let userId = this.getUserId(so.id);
 				let cupIndex = data.cupIndex;
-				let flag = ga.guessMouse(userId,cupIndex);
+				let flag = ga.guessMouse(userId, cupIndex);
 
-				so.emit(PushType.onguess,{
+				let gameId = ga.id;
+
+				io.to(gameId).emit(PushType.onguess, {
 					cupIndex,
-					isCorrect:ga.scoreList[ga.scoreList.length-1] == Score.win
+					isCorrect: ga.scoreList[ga.scoreList.length - 1] == Score.win
 				});
 
 				// 是否要全部推送总分
-				if(ga.isOver){
-					let userIdList = ga.userList.map(us=>us.id);
+				if (ga.isOver) {
+					let userIdList = ga.userList.map(us => us.id);
 					let result;
-					so.emit(PushType.onpublishScore,{
+					so.emit(PushType.onpublishScore, {
 						userIdList,
 						result
 					});
 				}
 			});
 
-			so.on('disconnection', () => { });
+			so.on('disconnection', () => {
+				this.leaveRoom(so.id);
+
+				let room = this.getRoom(so.id);
+				let gameId = room.gameId;
+				let userId = room.userId;
+				
+				io.to(gameId).emit(PushType.onleaveRoom,{
+					userId
+				});
+			});
 
 		});
-	}
-
-	// 查找或者新建room
-	private getGame(sid: string): Game {
-		let ga: Game;
-
-		return ga;
-	}
-
-
-	private getUserId(sid: string): string {
-		let userId: string;
-		return userId;
 	}
 
 
